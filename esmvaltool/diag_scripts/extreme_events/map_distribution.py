978c4a9ae57508f229c3f6ccce43a12cdc085d5a
Elizaveta Malinina <elizaveta.malinina-rieger@ec.gc.ca>
2024-02-15 02:02:53 +0000

    update map diag in accordance with recipe

--- esmvaltool/diag_scripts/extreme_events/map_distribution.py
+++ esmvaltool/diag_scripts/extreme_events/map_distribution.py
@@ -1,48 +1,75 @@
 import iris
 import iris.plot as iplt
+from iris.cube import Cube
 from iris.time import PartialDateTime
 import cartopy.crs as ccrs 
-import cartopy.feature as cff
 import geopandas as gpd 
 import esmvalcore.preprocessor as eprep
 from datetime import datetime, timedelta, date
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap
 import os
 
 
 # import internal esmvaltool modules here
 from esmvaltool.diag_scripts.shared import run_diagnostic, select_metadata, group_metadata, get_diagnostic_filename, save_data, ProvenanceLogger
 import esmvaltool.diag_scripts.shared.plot as eplot
-from esmvaltool.diag_scripts.ocean import diagnostic_tools as diagtools
-from esmvaltool.diag_scripts.shared import ProvenanceLogger
 
 # # This part sends debug statements to stdout
 logger = logging.getLogger(os.path.basename(__file__))
 # logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
-def extract_partial_dates(cube: iris.cube.Cube, inp_date: date, window: int):
+def obtain_cubes(dataset: list):
     '''
-    This function extracts the dates in +/- half window in each year.
+    This function retrieves current and reference cube from datasets.
+
+    Input: 
+        datasets: list with dictionaries based on ESMValTool input_data
+    Output:
+        current_cube: iris Cube with current data
+        ref_cube: iris Cube with reference data
+    '''
+
+    current_f = select_metadata(dataset,variable_group='current'
+                                                        )[0]['filename']
+    ref_f = select_metadata(dataset,variable_group='reference'
+                                                        )[0]['filename']
+    current_cb = iris.load_cube(current_f)
+    ref_cb = iris.load_cube(ref_f)
+
+    return current_cb, ref_cb
+
+
+def define_inp_date(cube: Cube, last_day: bool | None, inp_day: str | None):
+    '''
+    This function defines explicitly the inp_date
 
     Input:
-        cube: iris cube from which the data will be extracted
-        inp_date: date on which the extraction is centered
-        window: the half_window for extraction around the inp_date
-    Output:
-        short_cb: iris cube with extracted dates
+        cube: iris Cube with the data
+        last_day: boolean or None, defines if we should use the last day 
+        inp_day: str or None, date which will be used for analysis
+    Output: 
+        inp_date: date which will be used for analysis
     '''
-    min_bord = inp_date - timedelta(days=window)
-    max_bord = inp_date + timedelta(days=window)
-    min_constr = PartialDateTime(month=min_bord.month, day=min_bord.day)
-    max_constr = PartialDateTime(month=max_bord.month, day=max_bord.day)
-    pdt_constr = iris.Constraint(
-                    time = lambda cell: min_constr <= cell.point <= max_constr)
-    short_cb = cube.extract(pdt_constr)
 
-    return short_cb
+    if inp_day!=None:
+        inp_date = datetime.strptime(inp_day, '%Y-%m-%d').date()
+    
+    if last_day:
+        day_cb = cube[-1]
+        day_date = day_cb.coord('time').cell(0).point
+        if (inp_day!=None)&(inp_date.year==day_date.year
+                                )&(inp_date.month==day_date.month
+                                        )&(inp_date.day==day_date.day):
+            logger.warning(f'The input date {inp_day} from the recipe does '
+                   'not correspond to the last day in the observational cube. '
+                   'Proceeding with the last day.')
+        inp_date = datetime.strptime(
+            str(day_cb.coord('time').cell(0).point),'%Y-%m-%d %H:%M:%S').date()
+
+    return inp_date
+
 
 def plot_map(cube: iris.cube.Cube, dataset: str, cfg: dict):
     '''
@@ -64,22 +91,40 @@ def plot_map(cube: iris.cube.Cube, dataset: str, cfg: dict):
                            subplot_kw={'projection': ccrs.LambertConformal(
                                             central_longitude=-91.87,
                                             standard_parallels=(49,76))})
-    fig.set_size_inches(7., 7.)
+    fig.set_size_inches(7., 9.)
     fig.set_dpi(200)
 
     max_t = cube.collapsed(['latitude', 'longitude'], iris.analysis.MAX).data
     min_t = cube.collapsed(['latitude', 'longitude'], iris.analysis.MIN).data
-    bord = np.floor(np.abs(np.asarray([max_t, min_t]).flatten()).max()*0.975)
+    bord = np.floor(np.abs(np.asarray([max_t, min_t]).flatten()).max()*0.95)
     ax.coastlines(linewidth=0.5)
     map_pl = iplt.contourf(cube, axes=ax, levels=np.arange(-bord, bord+1,1), 
                                                 cmap='coolwarm', extend='both')
-    # add title
-    # add colorbar 
-    # adjust the location on the figure
-    # add the legend for the regions
+    ax.set_title('Maximum '+cfg['var_label'].lower()+' anomalies in ' + \
+              cfg['region'] + ' (' +str(datetime.strptime(str(
+              cube.coord('time').cell(0).point), '%Y-%m-%d %H:%M:%S').date())+\
+              ')', fontsize='x-large')
     shape_gpd.boundary.plot(color=None, edgecolor='grey', linewidth=0.5, ax=ax, 
-                                                  transform=ccrs.PlateCarree())
-    ax.set_extent([-60.5, -120.5,37.8,82.8])    
+                                                transform=ccrs.PlateCarree())
+    
+    ax.set_extent([-60.5, -120.5,37.8,82.8])  
+    ax.set_position([0.021, 0.315, 0.957, 0.64])
+
+    cax = fig.add_axes([0.117, 0.28, 0.78, 0.015])
+    fig.colorbar(map_pl, cax=cax, orientation='horizontal',
+                                              label='Temperature anomalies, C')
+
+    n_cols = 4 ; n_rows= int(np.ceil(len(shape_gpd)/n_cols))
+
+    for n in range(len(shape_gpd)):
+        ax.text(list(shape_gpd.iloc[n].geometry.centroid.coords)[0][0],
+                list(shape_gpd.iloc[n].geometry.centroid.coords)[0][1],
+                str(n+1), fontsize='x-large',  transform=ccrs.PlateCarree())
+        fig.text(0.021+(n//n_cols)*0.957/n_cols, 0.215-(n%n_rows)*0.2/n_rows, 
+                 str(n+1)+'. '+shape_gpd.iloc[n].ID)
+        
+
+    # add the legend for the regions  
     fig.savefig(os.path.join(cfg['plot_dir'], 
                                 f'map_canada_{dataset}.'+cfg['output_file_type']))
 
@@ -87,34 +132,31 @@ def plot_map(cube: iris.cube.Cube, dataset: str, cfg: dict):
 
 def main(cfg):
 
+    last_day_l = cfg.get('last_day'); inp_day = cfg.get('analysis_day')
+    if last_day_l== inp_day == None:
+        logger.error('Either last_day or analysis_day should be provided in '
+                                                                  'the recipe')
+
     input_data = cfg['input_data']
     
     datasets = group_metadata(input_data.values(), 'dataset', sort=True)
-    inp_date = datetime.strptime(cfg['analysis_day'], '%Y-%m-%d')
 
     for dataset in datasets.keys():
-        metadata = datasets[dataset]
-        filename = metadata[0]['filename']
-        obs_cb = iris.load_cube(filename)
-        last_day = obs_cb[-1]
-        last_date = last_day.coord('time').cell(0).point
-        if (inp_date.year==last_date.year)&(inp_date.month==last_date.month
-                                            )&(inp_date.day==last_date.day):
-            ref_cb = eprep.extract_time(obs_cb, 
-                                        start_year=cfg['reference_period'][0],
-                                        start_month=1, start_day=1,
-                                        end_year=cfg['reference_period'][1],
-                                        end_month=1, end_day=1)
-            ref_cb = extract_partial_dates(ref_cb, inp_date,cfg['half_window'])
-            ref_cb = eprep.annual_statistics(ref_cb, operator='max')
-            ref_cb = eprep.climate_statistics(ref_cb)
-            plot_cb = last_day - ref_cb
-            plot_map(plot_cb, dataset, cfg)
-            logger.info(f'Successfully processed {dataset}')
-        else:
-            logger.error('The input date '+cfg['analysis_day']+' from'
-                         ' the recipe does not correspond to the last day'
-                         ' in the observational cube')
+        
+        current_cb, ref_cb = obtain_cubes(datasets[dataset])
+
+        inp_date = define_inp_date(current_cb, last_day_l, inp_day)
+
+        day_cb = current_cb.extract(iris.Constraint(
+                    time=lambda cell: datetime.strptime(
+                    str(cell.point),'%Y-%m-%d %H:%M:%S').date()==inp_date))
+
+        day_of_y = inp_date.timetuple().tm_yday
+        ref_day_cb= ref_cb.extract(iris.Constraint(day_of_year=day_of_y))
+        
+        plot_cb = day_cb - ref_day_cb
+        plot_map(plot_cb, dataset, cfg)
+        logger.info(f'Successfully processed {dataset}')
 
     logger.info('Diagnostic completed successfully')
 
